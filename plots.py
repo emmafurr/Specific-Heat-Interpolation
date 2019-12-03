17b101ed2848a964a9986cb7d4f38f401a332140
nkalpakian <niko@br.iq>
2019-12-03 12:46:50 -0800

    Editing plot script

--- plots.py
+++ plots.py
@@ -1,5 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from final_project_script import nevillesMethod
+
+
+def nevillesMethod(x, list_x, list_fx, Q_table = None, individual = 'no',notable = 'no'):
+    n = np.size(list_x) - 1; 
+    if (Q_table == None):
+        Q_table = np.zeros((n + 1, n + 1));
+
+    for i in range(0,n + 1):
+        Q_table[i][0] = list_fx[i]
+
+    for i in range(1, n + 1):
+        for j in np.arange(1, i + 1):
+            Q_table[i][j] = 0.0
+            Q_table[i][j] += (((x - list_x[i - j])*Q_table[i][j - 1]- (x - list_x[i])*(Q_table[i - 1][j - 1]))/(list_x[i] - list_x[i - j]))
+    if individual == 'yes' and i == j:
+        print('Q_(',i,',',j,') =',Q_table[i][j])
+
+    if notable == 'yes':
+        return
+    return Q_table[n][n]
+
+# Nodes 1
+list_x = [20,40,50,80,90]
+list_fx = [4182,4179,4182,4198,4208]
+
+# Nodes 2
+list_x2 = [10,30,50,70,90]
+list_fx2 = [4192, 4178,4182, 4191, 4208]
+
 
 temperature_x = np.arange(0, 101)
 actual_specific_heat = 1000 * np.array([4.217, 4.213, 4.210, 4.207, 4.205, 4.202, 4.200, 4.198, 4.196,
@@ -43,6 +73,11 @@ for i in range(50, 80):
 for i in range(80, 90):
     cubic_spline_y_1_list.append(cubic_spline_1_3(temperature_x)[i])
 
+nevillesMethod_y_1_list = []
+for i in range(0, 101):
+    nevillesMethod_y_1_list.append(nevillesMethod(i, list_x, list_fx))
+
+
 cubic_spline_x_1_list = np.arange(20, 90)
 
 
@@ -56,9 +91,12 @@ lagrange_polynomial_1_values = lagrange_polynomial_1(temperature_x)
 # Plotting Functions
 plt.ylim(4170, 4230)
 plt.plot(temperature_x, actual_specific_heat, label="Actual")
-plt.plot(temperature_x, lagrange_polynomial_1_values, label="Cubic Spline")
-plt.plot(cubic_spline_x_1_list, cubic_spline_y_1_list, label="Lagrange Polynomial")
+plt.plot(temperature_x, lagrange_polynomial_1_values, label="Lagrange Polynomial")
+plt.plot(cubic_spline_x_1_list, cubic_spline_y_1_list, label="Cubic Spline")
+plt.plot(temperature_x, nevillesMethod_y_1_list, label="Neville's Method")
 plt.title("Using Data From Table 1")
+plt.xlabel("Temperature(°C)")
+plt.ylabel("Specific Heat (J / kg-°C)")
 plt.legend()
 plt.show()
 
@@ -100,6 +138,10 @@ for i in range(70, 90):
 
 cubic_spline_x_2_list = np.arange(10, 90)
 
+nevillesMethod_y_2_list = []
+for i in range(0, 101):
+    nevillesMethod_y_2_list.append(nevillesMethod(i, list_x2, list_fx2))
+
 
 # Lagrange Polynomial
 def lagrange_polynomial_2(x):
@@ -111,9 +153,12 @@ lagrange_polynomial_2_values = lagrange_polynomial_2(temperature_x)
 # Plotting Functions
 plt.ylim(4170, 4230)
 plt.plot(temperature_x, actual_specific_heat, label="Actual")
-plt.plot(temperature_x, lagrange_polynomial_2_values, label="Cubic Spline")
-plt.plot(cubic_spline_x_2_list, cubic_spline_y_2_list, label="Lagrange Polynomial")
+plt.plot(temperature_x, lagrange_polynomial_2_values, label="Lagrange Polynomial")
+plt.plot(cubic_spline_x_2_list, cubic_spline_y_2_list, label="Cubic Spline")
+plt.plot(temperature_x, nevillesMethod_y_2_list, label="Neville's Method")
 plt.title("Using Data From Table 2")
+plt.xlabel("Temperature(°C)")
+plt.ylabel("Specific Heat (J / kg-°C)")
 plt.legend()
 plt.show()
 
